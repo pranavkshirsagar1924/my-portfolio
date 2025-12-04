39f7197036cec9e5e417b3c6f97ce2d09c3c06ad
Pranav <128245885+pranavkshirsagar1924@users.noreply.github.com>
2025-12-04 15:01:29 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@
 
 export default function ContactPage() {
   return (
-    <section className="max-w-5xl mx-auto px-6 py-20 text-black">
+    <section className="max-w-5xl bg-white mx-auto px-6 py-20 text-black">
 
       {/* Title */}
       <div className="mb-14 text-center">
